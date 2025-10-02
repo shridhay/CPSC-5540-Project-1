9328483ab43ab2e1681c77f71cca489382488d54
Hridhay Suresh <shridhay@Hridhays-MacBook-Pro.local>
2025-10-02 13:16:23 -0400

    some stuff

--- Project_1/proj1/app/Main.hs
+++ Project_1/proj1/app/Main.hs
@@ -9,8 +9,12 @@ main :: IO ()
 main = do
     as <- getArgs
     prog <- readFile (head as)
-    result <- catchIOError (verify prog) (return . Unknown . show)
-    case result of
-      Verified -> putStrLn "Verified"
-      NotVerified -> putStrLn "Not verified"
-      Unknown msg -> putStrLn ("Verifier returned unknown: " ++ msg)
+    case (parseProg prog) of 
+      Just p -> print (getZ3String p)
+      Nothing -> print "Parsing failed"
+
+    -- result <- catchIOError (verify prog) (return . Unknown . show)
+    -- case result of
+    --   Verified -> putStrLn "Verified"
+    --   NotVerified -> putStrLn "Not verified"
+    --   Unknown msg -> putStrLn ("Verifier returned unknown: " ++ msg)
